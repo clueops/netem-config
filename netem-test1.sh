2dffe2068db0584e553a93c00e46098db16dd672
clueops <baron@clueops.com>
2020-03-19 21:57:51 +0000

    Testing for delays at hop 3

--- netem-test1.sh
+++ netem-test1.sh
@@ -12,10 +12,10 @@ sudo tc qdisc add dev $in_interface root handle 1: prio bands 2 priomap 1 1 1 1 
 sudo tc qdisc add dev $out_interface root handle 1: prio bands 2 priomap 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
 
 sudo tc qdisc add dev $in_interface parent 1:1 handle 10: pfifo
-sudo tc qdisc add dev $in_interface parent 1:2 handle 20: delay 50ms 20ms 25% distribution normal loss 5
+sudo tc qdisc add dev $in_interface parent 1:2 handle 20: netem delay 50ms 20ms 25% distribution normal loss 5
 
 sudo tc qdisc add dev $out_interface parent 1:1 handle 10: pfifo
-sudo tc qdisc add dev $out_interface parent 1:2 handle 20: delay 50ms 20ms 25% distribution normal loss 5
+sudo tc qdisc add dev $out_interface parent 1:2 handle 20: netem delay 50ms 20ms 25% distribution normal loss 5
 
 sudo tc filter add dev $in_interface protocol ip parent 1: prio 3 u32 match ip src 14.1.1.1/32 flowid 1:1
 sudo tc filter add dev $in_interface protocol ip parent 1: prio 3 u32 match ip src 11.1.1.70/32 flowid 1:1
