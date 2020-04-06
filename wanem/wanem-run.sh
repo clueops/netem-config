3dee8332108f5f61f7920782b0a247fd3506480e
clueops <baron@clueops.com>
2020-04-06 12:25:04 +0100

    WanEm add full conditions

--- wanem/wanem-run.sh
+++ wanem/wanem-run.sh
@@ -25,8 +25,12 @@ sudo tc qdisc add dev $in_interface root handle 1: prio bands 3 priomap 2 2 2 2 
 sudo tc qdisc add dev $out_interface root handle 1: prio bands 3 priomap 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
 
 # 1. Create LAN zone conditions
-    # First class (prior, id 0) uses normal queuing (fifo)
+    # First class (prior, id 0) uses normal queuing (fifo), no impairments
+
+    # Set new NetEm conditions on Inside interface (for inbound traffic)
     sudo tc qdisc add dev $in_interface parent 1:1 handle 10: pfifo
+
+    # Set new NetEm conditions on Outside interface (for outbound traffic)
     sudo tc qdisc add dev $out_interface parent 1:1 handle 10: pfifo
 
     # Traffic filters - loop through each IP in LAN zone
@@ -42,32 +46,47 @@ sudo tc qdisc add dev $out_interface root handle 1: prio bands 3 priomap 2 2 2 2
 
 # 2. Create WAN zone conditions
     # Second class (id 1) uses the specific impairment configuration (20:)
-    sudo tc qdisc add dev $in_interface parent 1:2 handle 20: netem delay $((wan_delay/2))ms
+
+    # Set new NetEm conditions on Inside interface (for inbound traffic)
+    if [ $wan_jitter = 0 ]
+    then
+        sudo tc qdisc add dev $in_interface parent 1:2 handle 20: netem delay $((wan_delay/2))ms loss $wan_loss
+    else
+		sudo tc qdisc add dev $in_interface parent 1:2 handle 20: netem delay $((wan_delay/2))ms $wan_jitter 25% distribution normal loss $wan_loss
+    fi
+
+    # Set new NetEm conditions on Outside interface (for outbound traffic)
     sudo tc qdisc add dev $out_interface parent 1:2 handle 20: netem delay $((wan_delay/2))ms
 
     # Traffic filters - loop through each IP in WAN zone
-    echo $wan_filters
     for wan_ip in $wan_filters
     do
-        echo $wan_ip
         # Apply traffic filters - Inbound
         sudo tc filter add dev $in_interface protocol ip parent 1: prio 2 u32 match ip src $wan_ip flowid 1:2
         # Apply traffic filters - Outbound
         sudo tc filter add dev $out_interface protocol ip parent 1: prio 2 u32 match ip dst $wan_ip flowid 1:2
     done
 
-# 3. Create WAN zone conditions
+# 3. Create Internet zone conditions
     # Third class (default, id 2) uses the specific impairment configuration (30:)
-    sudo tc qdisc add dev $in_interface parent 1:3 handle 30: netem delay $((int_delay/2))ms
+
+    # Set new NetEm conditions on Inside interface (for inbound traffic)
+    if [ $int_jitter = 0 ]
+    then
+        sudo tc qdisc add dev $in_interface parent 1:3 handle 30: netem delay $((int_delay/2))ms loss $int_loss
+    else
+		sudo tc qdisc add dev $in_interface parent 1:3 handle 30: netem delay $((int_delay/2))ms $int_jitter 25% distribution normal loss $int_loss
+    fi
+
+    # Set new NetEm conditions on Outside interface (for outbound traffic)
     sudo tc qdisc add dev $out_interface parent 1:3 handle 30: netem delay $((int_delay/2))ms
 
-    # Traffic filters - no need to apply a filter, this class is applied to all IPs not previously allocated to the LAN or WAN zones
+    # Traffic filters - no need to apply an Internet Zone filter, this is the default class and applied to all IPs not previously allocated to the LAN or WAN zones
 
+# Finish
 sudo tc qdisc show dev $in_interface
 sudo tc qdisc show dev $out_interface
 
-
-# Finish
 echo ""
 echo $(date -u)
 echo "Finished WanEm run!"
