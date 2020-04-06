0260015c7908c5917c5f7e0437f80e7ac921e14f
clueops <baron@clueops.com>
2020-04-06 11:05:42 +0100

    WanEm created

---
+++ wanem/wanem-config.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# wanem-config.sh
+# Parameter variables for use by 'wanem-run.sh' script
+
+# Set Interface to required network interface
+# Options: Inside [enp1s0f1], Outside: [enp1s0f0]
+in_interface=enp1s0f1
+out_interface=enp1s0f0
+
+# LAN Zone
+    # Conditions (device 10:)
+                            # No added behaviour
+    # Filters (class/flowid 1:1)
+        lan_filters="14.1.1.1/32 11.1.1.70/32"
+
+# WAN Zone
+    # Conditions (device 20:)
+        wan_delay="10"      # Specify value of Delay to use (in 'ms') - value will be applied 50%/50% to in and out
+        wan_jitter="0"      # Specify value of Jitter to use (in 'ms') - value will be applied 100% to in
+        wan_loss="0"        # Specify value of Loss to use (in '%') - value will be applied 100% to in
+    # Filters (class/flowid 1:2)
+        wan_filters="11.1.1.2/32 12.0.0.10/32 172.16.0.1/32"
+
+# Internet Zone
+    # Conditions (device 30:)
+        int_delay="10"      # Specify value of Delay to use (in 'ms') - value will be applied 50%/50% to in and out
+        int_jitter="0"      # Specify value of Jitter to use (in 'ms') - value will be applied 100% to in
+        int_loss="0"        # Specify value of Loss to use (in '%') - value will be applied 100% to in
+    # Filters (class/flowid 1:3)
+        int_filters=""      # default - everything else
---
+++ wanem/wanem-run.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# WanEm - wanem-run.sh
+# Script to create a NetEm environment to emulate a WAN
+
+: '
+ Script usage:
+ ./wanem-run.sh			        	: output to screen only
+ ./wanem-run.sh | tee <logfile.log>	: output to screen and log file <logfile.log>
+'
+
+# Load parameters file with required variables
+source wanem-config.sh
+
+# Start WanEm run here
+echo "Starting WanEm run..."
+echo $(date -u)
+
+# Remove any pre-existing active NetEm config
+sudo tc qdisc del dev $in_interface root
+sudo tc qdisc del dev $out_interface root
+
+# Create 3-class priority map (0,1,2), with all traffic redirected to the third class (id 2) by default.
+tc qdisc add dev $in_interface root handle 1: prio bands 3 priomap 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+tc qdisc add dev $out_interface root handle 1: prio bands 3 priomap 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+
+# Create LAN zone conditions
+    # First class (prior, id 0) uses normal queuing (fifo)
+    sudo tc qdisc add dev $in_interface parent 1:1 handle 10: pfifo
+    sudo tc qdisc add dev $out_interface parent 1:1 handle 10: pfifo
+
+    # Traffic filters - loop through each IP in LAN zone
+    for lan_ip in $lan_filters
+    do
+        # Apply traffic filters - Inbound
+        sudo tc filter add dev $in_interface protocol ip parent 1: prio 3 u32 match ip src $lan_ip flowid 1:1
+        # Apply traffic filters - Outbound
+        sudo tc filter add dev $out_interface protocol ip parent 1: prio 3 u32 match ip dst $lan_ip flowid 1:1
+    done
+
+    sudo tc qdisc show dev $in_interface
+    sudo tc qdisc show dev $out_interface
+
+# Finish
+echo ""
+echo $(date -u)
+echo "Finished WanEm run!"
+
