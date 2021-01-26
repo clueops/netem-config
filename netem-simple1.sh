2beb51747c363c3e01a864ba2a813b9acc4ad088
clueops <baron@clueops.com>
2021-01-26 17:41:39 +0000

    added sudo to tc commands

--- netem-simple1.sh
+++ netem-simple1.sh
@@ -5,16 +5,16 @@
 # Useing Inside interface only (for inbound traffic)
 
 # Clear any existing qdisc devices
-/sbin/tc qdisc del dev enp1s0f1 root
+sudo /sbin/tc qdisc del dev enp1s0f1 root
 
 # Create 2-class priority map (0,1), with all traffic redirected to the second class (id 1, qdisc 1:2) by default.
-/sbin/tc qdisc add dev enp1s0f1 root handle 1: prio bands 2 priomap 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+sudo /sbin/tc qdisc add dev enp1s0f1 root handle 1: prio bands 2 priomap 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
 
 # Add a new NetEm qdisc 1:1 with no impairments
-/sbin/tc qdisc add dev enp1s0f1 parent 1:1 handle 10: pfifo limit 1000
+sudo /sbin/tc qdisc add dev enp1s0f1 parent 1:1 handle 10: pfifo limit 1000
 
 # Add a new NetEm qdisc 1:2 with delay and loss impairments on Inside interface (for inbound traffic)
-/sbin/tc qdisc add dev enp1s0f1 parent 1:2 handle 20: netem delay 10ms 10ms 25% distribution normal loss 25
+sudo /sbin/tc qdisc add dev enp1s0f1 parent 1:2 handle 20: netem delay 10ms 10ms 25% distribution normal loss 25
 
 # Default behaviour is all traffic is sent to qdisc 1:2 and is impaired
 
@@ -28,6 +28,6 @@
 sleep 3000
 
 # Remove qdisc devices
-/sbin/tc qdisc del dev enp1s0f1 root
+sudo /sbin/tc qdisc del dev enp1s0f1 root
 
 #end
